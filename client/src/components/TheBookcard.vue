1ebc76128e5e68fadf699c9508b97b83256d6667
조성한 <toor21@naver.com>
2022-01-17 23:37:23 +0900

    feat: bookcard (experimental)

---
+++ client/src/components/TheBookcard.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card
+    class="TheBookcard"
+    elevation="2">
+    {{ book.title }}
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'TheBookcard',
+
+  // props: {
+  //   book: String,
+  // },
+
+  data: function () {
+    return {
+      book: {
+        title: "주기율표"
+      },
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
