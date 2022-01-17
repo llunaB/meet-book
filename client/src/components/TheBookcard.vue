b7ebe2188fec42a54c99eb78f9ad132e2e2923f4
조성한 <toor21@naver.com>
2022-01-17 23:48:06 +0900

    feat: bookcard (experimental)

--- client/src/components/TheBookcard.vue
+++ client/src/components/TheBookcard.vue
@@ -1,8 +1,15 @@
 <template>
   <v-card
     class="TheBookcard"
+    max-width="400"
     elevation="2">
-    {{ book.title }}
+    <v-card-title>
+      {{ book.title }}
+    </v-card-title>
+    <v-card-text>
+      <p>텍스트 연습입니다.</p>
+      <p>{{ book.author }}</p>
+    </v-card-text>
   </v-card>
 </template>
 
@@ -17,7 +24,8 @@ export default {
   data: function () {
     return {
       book: {
-        title: "주기율표"
+        title: "주기율표",
+        author: "프리모 레비",
       },
     }
   },
