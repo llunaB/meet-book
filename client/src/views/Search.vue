f4dcc7859231aacca2f874c57a72ea91795f4881
조성한 <toor21@naver.com>
2022-01-19 16:04:24 +0900

    feat: add Search

---
+++ client/src/views/Search.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-row class="searchtabs">
+      <v-tabs>
+        <v-tab>회의</v-tab>
+        <v-tab>도서</v-tab>
+        <v-tab>사용자</v-tab>
+      </v-tabs>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-tabs-items>
+          <v-tab-item
+            v-for="i in 3"
+            :key="i"
+            :value="`tab-${i}`">
+          </v-tab-item>
+
+        </v-tabs-items>
+      </v-col>
+      <v-col>
+
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+
+}
+</script>
+
+<style>
+
+</style>
