b952cd97fd58ba7e69578c582c60cdf76559a0de
정찬 <cjung0418@naver.com>
2022-01-20 13:27:05 +0900

    feat: home_page conference_slide

---
+++ client/src/components/home/ConferenceSlide.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <v-sheet>
+    <v-slide-group show-arrows>
+      <v-slide-item v-for="conference in conferences" :key="conference.id" class="col-1 ma-3">
+        <conference-card :conference="conference">
+        </conference-card>        
+      </v-slide-item>
+    </v-slide-group>
+  </v-sheet>  
+</div>
+  
+</template>
+<script>
+import ConferenceCard from './ConferenceCard'
+
+export default {
+  name: "ConferenceList",
+  props: {
+    conferences: Array,
+  },
+  components: {
+    ConferenceCard
+  },  
+}
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,36 @@
 <template>
-  <hello-world />
+  <div class="container">
+    <carousel>
+    </carousel>
+    <h2 class="my-3">지금 가장 핫한 모임</h2>
+    <conference-slide :conferences="conferences">
+    </conference-slide>    
+  </div>  
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  // import HelloWorld from '../components/HelloWorld'
+  import Carousel from '../components/home/Carousel'
+  import ConferenceSlide from '../components/home/ConferenceSlide'
 
   export default {
     name: 'Home',
-
+    data(){
+      return {
+        conferences: [{"id":1, "title":"오이디푸스", "thumbnail":"https://image.yes24.com/momo/TopCate393/MidCate005/6417738.jpg","description":"Welcome","isActive":true},
+        {"id":2, "title":"정의란 무엇인가", "thumbnail":"https://image.yes24.com/goods/15156691/XL", "description":"welcome2","isActive":true},
+        {"id":3, "title":"오이디푸스", "thumbnail":"https://image.yes24.com/momo/TopCate393/MidCate005/6417738.jpg","description":"Welcome","isActive":true},
+        {"id":4, "title":"정의란 무엇인가", "thumbnail":"https://image.yes24.com/goods/15156691/XL", "description":"welcome2","isActive":false},
+        {"id":5, "title":"오이디푸스", "thumbnail":"https://image.yes24.com/momo/TopCate393/MidCate005/6417738.jpg","description":"Welcome", "isActive":false},
+        {"id":6, "title":"정의란 무엇인가", "thumbnail":"https://image.yes24.com/goods/15156691/XL", "description":"welcome2", "isActive":false},
+        {"id":7, "title":"오이디푸스", "thumbnail":"https://image.yes24.com/momo/TopCate393/MidCate005/6417738.jpg","description":"Welcome", "isActive":false},
+        {"id":8, "title":"정의란 무엇인가", "thumbnail":"https://image.yes24.com/goods/15156691/XL", "description":"welcome2", "isActive":false},]
+        
+      }
+    },
     components: {
-      HelloWorld,
+      ConferenceSlide,
+      Carousel
     },
   }
 </script>
