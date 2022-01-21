93433e05fd879d0150e15751d2fe46baed983f95
조성한 <toor21@naver.com>
2022-01-21 13:19:45 +0900

    feat: add Search

--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -1,35 +1,44 @@
 <template>
   <div>
     <v-row class="searchtabs">
-      <v-tabs>
+      <v-tabs
+      color="#798F88">
+        <v-tabs-slider color="#798F88"></v-tabs-slider>
         <v-tab
+          @click="function () {searchType = 'conf'}"
           :to="`conf?keyword=${keyword}`">회의</v-tab>
         <v-tab
+          @click="function () {searchType = 'book'}"
           :to="`book?keyword=${keyword}`">도서</v-tab>
         <v-tab
+          @click="function() {searchType = 'users'}"
           :to="`users?keyword=${keyword}`">사용자</v-tab>
       </v-tabs>
     </v-row>
     <v-row>
       <v-col class="col-9">
-        <v-tabs-items>
-          <v-tab-item
-            v-for="i in 3"
-            :key="i"
-            :value="`tab-${i}`">
-          </v-tab-item>
+        <div v-if="searchType == 'conf'">
+          <p>회의 검색 결과입니다.</p>
+          
+        </div>
+        <div v-else-if="searchType == 'book'">
+          <p>도서 검색 결과입니다.</p>
+        </div>
+        <div v-else>
+          <p>사용자 검색 결과입니다.</p>
+        </div>
 
-        </v-tabs-items>
       </v-col>
       <v-col class="col-3">
-        <p>"{{ keyword }}"에 대해 {{searchType}}로 검색한 결과입니다.</p>
-        <p>{{ $route.query.title}}</p>
+        <p>"{{ $route.query.keyword }}"에 대해 {{$route.params.type}}로 검색한 결과입니다.</p>
+        <p>{{ $route.query.title }}</p>
       </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'Search',
   data: function () {
@@ -40,9 +49,24 @@ export default {
     }
   },
   methods: {
+    search: function (type, keyword) {
+      console.log(type, keyword)
+      axios({
+        url: '',
+        method: 'get',
+        params: '',
+      })
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    },
+
 
   },
-  
+
 }
 </script>
 
