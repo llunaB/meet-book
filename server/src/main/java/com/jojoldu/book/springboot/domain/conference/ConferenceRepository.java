a27119057a885ebc63a511f8b7a3bd2c84a36d38
euijinbang <euijin.bang@gmail.com>
2022-01-27 15:26:43 +0900

    chore : delete no-use server

--- server/src/main/java/com/jojoldu/book/springboot/domain/conference/ConferenceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jojoldu.book.springboot.domain.conference;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConferenceRepository extends JpaRepository<Conference, Integer> {
-}
--- server/src/test/java/com/jojoldu/book/springboot/domain/user/UserTest.java
+++
@@ -1,7 +0,0 @@
-package com.jojoldu.book.springboot.domain.user;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class UserTest {
-
-}
