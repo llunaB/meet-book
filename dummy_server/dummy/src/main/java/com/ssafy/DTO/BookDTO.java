79c4a0d70e7b4f4e08c020bce640422aca16f86e
euijinbang <euijin.bang@gmail.com>
2022-01-28 13:00:18 +0900

    refactor : OpenApiHelper to OpenApiService

--- dummy_server/dummy/src/main/java/com/ssafy/DTO/BookDTO.java
+++ dummy_server/dummy/src/main/java/com/ssafy/DTO/BookDTO.java
@@ -2,10 +2,7 @@ package com.ssafy.DTO;
 
 import java.util.Date;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
--- dummy_server/dummy/src/main/java/com/ssafy/DummyApplication.java
+++ dummy_server/dummy/src/main/java/com/ssafy/DummyApplication.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ssafy.db.openApi.OpenApiHelper;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
