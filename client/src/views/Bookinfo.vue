b5069966d65e0c0ac636fc862a3a821c3a17e4bf
조성한 <toor21@naver.com>
2022-02-07 14:54:50 +0900

    feat: add bookinfo

--- client/src/views/Bookinfo.vue
+++ client/src/views/Bookinfo.vue
@@ -4,24 +4,24 @@
       <!-- 도서 정보 탑재 부분 -->
       <div class="row">
         <div class="d-flex col-12 col-sm-4 col-lg-12 text-center justify-center">
-          <v-img v-if="book.bookThumbnailUrl" :src="book.bookThumbnailUrl" max-width="240px"></v-img>
+          <v-img v-if="book.thumbnailUrl" :src="book.thumbnailUrl" max-width="240px"></v-img>
           <span v-else>썸네일 이미지가 없습니다.</span>
         </div>
         <div class="col-12 col-sm-8 col-lg-12">
-          <h1>{{ book.bookName }}</h1>
-          <h3>{{ book.bookAuthor }}</h3>
-          <h5 class="text-right">{{ book.bookPublisher }} | {{ book.bookPubYear }}</h5>
+          <h1>{{ book.name }}</h1>
+          <h3>{{ book.author }}</h3>
+          <h5 class="text-right">{{ book.publisher }} | {{ book.pubYear }}</h5>
           <v-divider light class="my-5" />
           <p class="caption text--secondary">ISBN {{ book.isbn }}</p>
-          <p>{{ book.bookContents }}</p>
-          <p v-show="!book.bookContents">현재 책 내용이 제공되지 않습니다.</p>
+          <p>{{ book.contents }}</p>
+          <p v-show="!book.contents">현재 책 내용이 제공되지 않습니다.</p>
         </div>
       </div>
       
     </div>
     <!-- 도서 관련 회의 탑재 부분 -->
     <div class="col-12 col-lg-6">
-      <h3>{{ book.title }}을 읽은/읽을 모임</h3> <br>
+      <h3>{{ book.name }}을 읽은/읽을 모임</h3> <br>
 
       <div v-if="conferences.length == 0">
         <div class="text-center">
@@ -35,7 +35,7 @@
     </div>
       
     <div class="col-12">
-      <h3>{{ book.title }}을 읽은 사람들</h3> <br>
+      <h3>{{ book.name }}을 읽은 사람들</h3> <br>
         <div v-if="people.length == 0">
           <div class="text-center">
             <p>아직 이 책에 대해 이야기 나눈 사람이 없어요!</p>
@@ -64,13 +64,13 @@ export default {
     return {
       book: {
         id: 1,
-        bookName: '책 제목',
-        bookAuthor: '작가',
-        bookContents: '여기는 책의 설명이 들어가는 부분입니다. 적당히 길 수도 있습니다.',
-        bookPublisher: '출판사',
-        isbn: '9791190893442',
-        bookPubYear: '2000',
-        bookThumbnailUrl: 'https://image.aladin.co.kr/product/25241/8/cover/k872633007_1.jpg',
+        name: '책 제목',
+        author: '작가',
+        contents: '여기는 책의 설명이 들어가는 부분입니다. 적당히 길 수도 있습니다.',
+        publisher: '출판사',
+        isbn: '00000000000000',
+        pubYear: '1900',
+        thumbnailUrl: 'https://image.aladin.co.kr/product/25241/8/cover/k872633007_1.jpg',
       },
       // conferences: [{"id":1, "title":"오이디푸스", "thumbnail":"https://image.yes24.com/momo/TopCate393/MidCate005/6417738.jpg","description":"Welcome","isActive":true},
       //   {"id":2, "title":"정의란 무엇인가", "thumbnail":"https://image.yes24.com/goods/15156691/XL", "description":"welcome2","isActive":true},
@@ -105,7 +105,6 @@ export default {
         url: `books/${bookId}`,
       })
       .then(response => {
-        console.log(response.data)
         this.book = response.data
       })
       .catch(error => {
