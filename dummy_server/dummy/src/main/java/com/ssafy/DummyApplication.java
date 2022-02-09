8d9d6924fa2b281cac5a6629c9f4e42bf9891736
권도혁 <fsa3806@naver.com>
2022-02-09 11:02:35 +0900

    feature:add send email to user who turn on alarm

--- dummy_server/dummy/src/main/java/com/ssafy/DummyApplication.java
+++ dummy_server/dummy/src/main/java/com/ssafy/DummyApplication.java
@@ -4,9 +4,11 @@ import com.ssafy.api.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
+@EnableScheduling
 @SpringBootApplication
 public class DummyApplication {
 
