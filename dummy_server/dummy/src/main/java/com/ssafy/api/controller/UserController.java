69ec2fd0353af12c2f01c61f6fe7c866af2cc693
euijinbang <euijin.bang@gmail.com>
2022-02-12 03:42:26 +0900

    chore : delete unusing import

--- dummy_server/dummy/src/main/java/com/ssafy/api/controller/UserController.java
+++ dummy_server/dummy/src/main/java/com/ssafy/api/controller/UserController.java
@@ -8,8 +8,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
