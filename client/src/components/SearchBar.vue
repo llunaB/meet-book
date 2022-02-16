0e3b582fda469ff8f007120940486b0c50f3eccb
Eonyong <unjoo94@naver.com>
2022-02-16 14:40:35 +0900

    edit(front): ( edit

--- client/src/components/SearchBar.vue
+++ client/src/components/SearchBar.vue
@@ -42,9 +42,10 @@ export default {
         // Navbar에서의 검색은 실제 검색을 수행하는 것이 아니라,
         // 검색 페이지에 검색 키워드로 전달하는 방식으로 수행됩니다.
         searchKeyword: function (keyword) {
-        this.$router.push({name: 'Search', params: {'type': 'conference'}, query: {'keyword': String(keyword), 'page': parseInt(1)}})
+          this.$router.push({name: 'Search', params: {'type': 'conference'}, query: {'keyword': String(keyword), 'page': parseInt(1)}})
         },
     }
+  }
 }
 </script>
 
