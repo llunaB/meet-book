1338785e37922a811d92d9256033a75c171d99c9
조성한 <toor21@naver.com>
2022-02-16 14:10:25 +0900

    feat: add bookinfo

--- client/src/views/Bookinfo.vue
+++ client/src/views/Bookinfo.vue
@@ -1,49 +1,51 @@
 <template>
-  <div class="row pa-1">
-    <div class="col-12 col-lg-6">
-      <!-- 도서 정보 탑재 부분 -->
-      <div class="row">
-        <div class="d-flex col-12 col-sm-4 col-lg-12 text-center justify-center">
-          <v-img v-if="book.thumbnailUrl" :src="book.thumbnailUrl" max-width="240px"></v-img>
-          <span v-else>썸네일 이미지가 없습니다.</span>
-        </div>
-        <div class="col-12 col-sm-8 col-lg-12">
-          <h1>{{ book.name }}</h1>
-          <h3>{{ book.author }}</h3>
-          <h5 class="text-right">{{ book.publisher }} | {{ book.pubYear }}</h5>
-          <v-divider light class="my-5" />
-          <p class="caption text--secondary">ISBN {{ book.isbn }}</p>
-          <p>{{ book.contents }}</p>
-          <p v-show="!book.contents">현재 책 내용이 제공되지 않습니다.</p>
-        </div>
-      </div>
-      
-    </div>
-    <!-- 도서 관련 회의 탑재 부분 -->
-    <div class="col-12 col-lg-6">
-      <h3>{{ book.name }}을 읽은/읽을 모임</h3> <br>
-
-      <div v-if="conferences.length === 0">
-        <div class="text-center">
-          <p>아직 이 책에 관한 모임이 없어요!</p>
-          <router-link :to="{name: 'CreateConference', query: {bookId: book.id}}">지금 이 책 모임 만들기</router-link>
-        </div>
-      </div>
-
-      <ConferenceSlide v-else :conferences="conferences" />
-
-    </div>
-      
-    <div class="col-12">
-      <h3>{{ book.name }}을 읽은 사람들</h3> <br>
-        <div v-if="users.length == 0">
-          <div class="text-center">
-            <p>아직 이 책에 대해 이야기 나눈 사람이 없어요!</p>
+  <div class="container">
+    <div class="row pa-1">
+      <div class="col-12 col-lg-6">
+        <!-- 도서 정보 탑재 부분 -->
+        <div class="row">
+          <div class="d-flex col-12 col-sm-4 col-lg-12 text-center justify-center">
+            <v-img v-if="book.thumbnailUrl" :src="book.thumbnailUrl" max-width="240px"></v-img>
+            <span v-else>썸네일 이미지가 없습니다.</span>
+          </div>
+          <div class="col-12 col-sm-8 col-lg-12">
+            <h1>{{ book.name }}</h1>
+            <h3>{{ book.author }}</h3>
+            <h5 class="text-right">{{ book.publisher }} | {{ book.pubYear }}</h5>
+            <v-divider light class="my-5" />
+            <p class="caption text--secondary">ISBN {{ book.isbn }}</p>
+            <p>{{ book.contents }}</p>
+            <p v-show="!book.contents">현재 책 내용이 제공되지 않습니다.</p>
           </div>
         </div>
-        <div v-else>
-          <ProfileSmallcard v-for="(person, idx) in users" :key="idx" :person="person" />
+        
+      </div>
+      <!-- 도서 관련 회의 탑재 부분 -->
+      <div class="col-12 col-lg-6">
+        <h3>{{ book.name }}을 읽은/읽을 모임</h3> <br>
+
+        <div v-if="conferences.length === 0">
+          <div class="text-center">
+            <p>아직 이 책에 관한 모임이 없어요!</p>
+            <router-link :to="{name: 'CreateConference', query: {bookId: book.id}}">지금 이 책 모임 만들기</router-link>
+          </div>
         </div>
+
+        <ConferenceSlide v-else :conferences="conferences" />
+
+      </div>
+        
+      <div class="col-12">
+        <h3>{{ book.name }}을 읽은 사람들</h3> <br>
+          <div v-if="users.length == 0">
+            <div class="text-center">
+              <p>아직 이 책에 대해 이야기 나눈 사람이 없어요!</p>
+            </div>
+          </div>
+          <div v-else>
+            <ProfileSmallcard v-for="(person, idx) in users" :key="idx" :person="person" />
+          </div>
+      </div>
     </div>
   </div>
     
@@ -60,11 +62,14 @@ export default {
   components: {
     ConferenceSlide, ProfileSmallcard,
   },
+
   data: function () {
     return {
       book: {
         id: 0,
-        name: '',
+        name: '로드 중입니다...',
+        nameFront: undefined,
+        nameBack: undefined,
         author: '',
         contents: '',
         publisher: '',
@@ -97,7 +102,10 @@ export default {
         url: `books/${bookId}`,
       })
       .then(response => {
-        this.book = response.data
+        const book = response.data
+        const [a, b] = this.getSlicedName(book.name)
+        this.book.nameFront = a
+        this.book.nameBack = b
       })
       .catch(error => {
         console.log(error)
@@ -114,7 +122,6 @@ export default {
       })
       .then(response => {
         // this.book = response.data.book
-        console.log(response)
         this.conferences = response.data.content
       })
       .catch(error => {
@@ -122,6 +129,14 @@ export default {
       })
     },
 
+    getSlicedName: function (String) {
+      const slicePoint = String.indexOf(':')
+      if (slicePoint === -1) {
+        return [undefined, undefined]
+      } else {
+        return [String.slice(0, slicePoint), String.slice(slicePoint)]
+      }
+    }
 
   },
 
