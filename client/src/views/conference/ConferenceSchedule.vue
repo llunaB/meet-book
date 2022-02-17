285107c6ec90afa2754f9ef3af384a67f01489e4
조성한 <toor21@naver.com>
2022-02-17 16:38:21 +0900

    feat: add bookmarked to conf schedule

--- client/src/views/conference/ConferenceSchedule.vue
+++ client/src/views/conference/ConferenceSchedule.vue
@@ -6,20 +6,20 @@
       </h3>
     </div>
 
-    <!-- <div class="col-12 col-md-10 offset-md-1">
+    <div class="col-12 col-md-10 offset-md-1">
       <p>북마크한 모임</p>
       
-      <div v-show="bookmarkedConference.length === 0"><p>현재 북마크한 모임이 없습니다. <br> <router-link :to="{name: 'ConferenceIndex'}">모임 찾아보기</router-link></p></div>
-      <div v-if="bookmarkedConference.length > 0">
-        <ConferenceCard class="mx-2 my-2 conf-card" v-for="conference in bookmarkedConference" :key="conference.id" :conference="conference" />
+      <div v-show="bookmarkedConference.numberOfElements === 0"><p>현재 북마크한 모임이 없습니다. <br> <router-link :to="{name: 'ConferenceIndex'}">모임 찾아보기</router-link></p></div>
+      <div v-if="bookmarkedConference.numberOfElements > 0" class="d-flex flex-row flex-wrap justify-space-around">
+        <ConferenceCard class="mx-2 my-2 conf-card" v-for="(conference, idx) in bookmarkedConference.content" :key="idx" :conference="conference" />
       </div>
-    </div> -->
+    </div>
 
     <div class="col-12 col-md-10 offset-md-1">
       <p>개설한 모임</p>
       <div v-show="createdConference.numberOfElements === 0"><p>아직 개설한 모임이 없습니다. <br> <router-link :to="{name: 'CreateConference'}">모임 만들기</router-link></p></div>
       <div v-if="createdConference.numberOfElements > 0" class="d-flex flex-row flex-wrap justify-space-around">
-        <ConferenceCard class="mx-2 my-2 conf-card" v-for="conference in createdConference.content" :key="conference.id" :conference="conference" />
+        <ConferenceCard class="mx-2 my-2 conf-card" v-for="(conference, idx) in createdConference.content" :key="idx" :conference="conference" />
       </div>
     </div>
     
@@ -27,7 +27,7 @@
       <p>참여한 모임</p>
       <div v-show="joinedConference.numberOfElements === 0"><p>아직 참여한 모임이 없습니다. <br> <router-link :to="{name: 'ConferenceIndex'}">모임 찾아보기</router-link></p></div>
       <div v-if="joinedConference.numberOfElements > 0" class="d-flex flex-row flex-wrap justify-space-around">
-        <ConferenceCard class="mx-2 my-2 conf-card" v-for="conference in joinedConference.content" :key="conference.id" :conference="conference" />
+        <ConferenceCard class="mx-2 my-2 conf-card" v-for="(conference, idx) in joinedConference.content" :key="idx" :conference="conference" />
       </div>
     </div>
 
@@ -47,9 +47,9 @@ export default {
   data: function () {
     return {
       // 표시할 세 가지 정보
-      bookmarkedConference: [],
-      createdConference: [],
-      joinedConference: [],
+      bookmarkedConference: {},
+      createdConference: {},
+      joinedConference: {},
     }
   },
   methods: {
@@ -58,7 +58,7 @@ export default {
         method: 'GET',
         baseURL: SERVER_URL,
         headers: {'X-AUTH-TOKEN': this.auth.user.token},
-        url: `/users/${userId}/bookmark`
+        url: `conference/bookmarked?id=${userId}&page=0&size=4`
       })
       .then(response => {
         // 북마크한 회의 담기
